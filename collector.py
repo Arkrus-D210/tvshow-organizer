5ffd880c3aa21cf494149227e578509cef5f9148
Arkrus <g.konstantelos@gmail.com>
2022-02-02 22:15:58 -0500

    ran it through autopep8

--- collector.py
+++ collector.py
@@ -9,22 +9,26 @@ from datetime import datetime
 import requests
 import json
 
+
 def log_discord(*args):
     log = ' '.join(map(str, args))
     webhook_url = DISCORD_WEBHOOK
     slack_data = {'content': "%s" % log}
-    response = requests.post(webhook_url, data=json.dumps(slack_data),headers={'Content-Type': 'application/json'})
+    response = requests.post(webhook_url, data=json.dumps(
+        slack_data), headers={'Content-Type': 'application/json'})
+
 
 def is_a_series(file):
     series = file.split(".")
     for segment in series:
         if re.match("[a-z][0-9][0-9][a-z][0-9][0-9]+", segment.lower()):
             return True
-    return False  
+    return False
+
 
 def series_name(file):
     series = file.split(".")
-    name =""
+    name = ""
     isASeries = False
     for segment in series:
         if re.match("[a-z][0-9][0-9][a-z][0-9][0-9]+", segment.lower()):
@@ -38,13 +42,15 @@ def series_name(file):
         name = TITLE_DB.get(name)
     return name
 
+
 def series_season(file):
     series = file.split(".")
     if is_a_series(file):
         for segment in series:
             if re.match("[a-z][0-9][0-9][a-z][0-9][0-9]+", segment.lower()):
-                season = int(segment.lower().split("e")[0].replace("s",""))
+                season = int(segment.lower().split("e")[0].replace("s", ""))
                 return f"Season {season}"
+
 
 def series_folder(name):
     if name != None:
@@ -55,30 +61,34 @@ def series_folder(name):
                     return f"{drive}\\{item}"
     return None
 
+
 def choose_drive():
     drive_sizes = {}
     for drive in RESOURCE_DRIVE:
         total, used, free = shutil.disk_usage(drive)
-        drive_sizes.update({drive:free})
+        drive_sizes.update({drive: free})
     largest_free_space = 0
     drive = ""
     for item in drive_sizes.items():
         if item[1] > largest_free_space:
             largest_free_space = item[1]
             drive = item[0]
-    return drive   
+    return drive
+
 
 def find_file(name, dirlist):
     for path in dirlist:
         if name in path:
-            return path    
+            return path
+
 
 def absoluteFilePaths(directory):
     directory_list = []
-    for dirpath,_,filenames in os.walk(directory):
+    for dirpath, _, filenames in os.walk(directory):
         for f in filenames:
             directory_list.append(os.path.abspath(os.path.join(dirpath, f)))
     return directory_list
+
 
 def generate_title():
     clear()
@@ -91,10 +101,12 @@ def generate_title():
     print("        \\/                      \\/     \\/                   |")
     print("+--------------------------------------------------By Arkrus+")
 
+
 def main():
     file_list = []
-    #Generate inventory
-    t = PrettyTable(["File","Title","Season","Source Directory","Target Directory","Season Directory Exists"])
+    # Generate inventory
+    t = PrettyTable(["File", "Title", "Season", "Source Directory",
+                    "Target Directory", "Season Directory Exists"])
     log_discord(f"Process Started at {datetime.now()}")
     for drive in SOURCE_DRIVE:
         dirlist = absoluteFilePaths(SOURCE_DRIVE[0])
@@ -105,8 +117,9 @@ def main():
                         title = series_name(name)
                         season = series_season(name)
                         target_directory = series_folder(title)
-                        source_directory = find_file(name,dirlist)
-                        season_directory = os.path.isdir(f"{target_directory}\\{season}")
+                        source_directory = find_file(name, dirlist)
+                        season_directory = os.path.isdir(
+                            f"{target_directory}\\{season}")
                         payload = \
                             {
                                 "title": title,
@@ -116,53 +129,60 @@ def main():
                                 "season_directory": season_directory,
                                 "filename": name
                             }
-                        t.add_row([name,title,season,source_directory,target_directory,season_directory])
+                        t.add_row([name, title, season, source_directory,
+                                  target_directory, season_directory])
                         file_list.append(payload)
-    #Create directory folders
+    # Create directory folders
     t.align = "l"
-    t._max_width = {"Source Directory" : 25,"Target Directory" : 25}
+    t._max_width = {"Source Directory": 25, "Target Directory": 25}
     print(t)
     tvshows = 0
     other = 0
     for file in file_list:
         if file["title"] != None:
-            tvshows +=1  
+            tvshows += 1
         else:
-            other +=1
+            other += 1
     log_discord(f"{tvshows} TV shows Found")
-    log_discord(f"{other} additional media files Found")     
+    log_discord(f"{other} additional media files Found")
     if CREATE_FOLDERS:
         print("Creating directories")
         for file in file_list:
             print(json.dumps(file, indent=4))
-            if file["title"] != None:           
+            if file["title"] != None:
                 if file["target_directory"] == None:
-                    #check if share has been created
+                    # check if share has been created
                     location = series_folder(file["title"])
                     if location == None:
                         drive = choose_drive()
-                        #create folder
+                        # create folder
                         title = file["title"]
                         os.mkdir(f"{drive}\\{title}")
                     file["target_directory"] = f"{drive}\\{title}"
                     log_discord(f'{file["target_directory"]} has been created')
                 if not file["season_directory"]:
-                    #check season has been created
+                    # check season has been created
                     print(f"Target directory is {file['target_directory']}")
-                    print(f"With a full path of {file['target_directory']}\\{file['season']}")
-                    season_directory = os.path.isdir(f"{file['target_directory']}\\{file['season']}")
+                    print(
+                        f"With a full path of {file['target_directory']}\\{file['season']}")
+                    season_directory = os.path.isdir(
+                        f"{file['target_directory']}\\{file['season']}")
                     if season_directory == False:
-                        os.mkdir(f"{file['target_directory']}\\{file['season']}")
-                        log_discord(f"{file['target_directory']}\\{file['season']} has been created")
-                #move file
+                        os.mkdir(
+                            f"{file['target_directory']}\\{file['season']}")
+                        log_discord(
+                            f"{file['target_directory']}\\{file['season']} has been created")
+                # move file
                 print(f"Moving {file['filename']}")
-                shutil.move(file["source_directory"],f"{file['target_directory']}\\{file['season']}\\{file['filename']}")
+                shutil.move(
+                    file["source_directory"], f"{file['target_directory']}\\{file['season']}\\{file['filename']}")
                 print(f"{file['filename']} Moved")
             else:
                 print(f"Moving {file['filename']}")
-                shutil.move(file["source_directory"],f"{UNSORTED_DRIVE[0]}\\{file['filename']}")
+                shutil.move(file["source_directory"],
+                            f"{UNSORTED_DRIVE[0]}\\{file['filename']}")
                 print(f"{file['filename']} Moved")
-    log_discord("Run Complete")     
+    log_discord("Run Complete")
 
 
 if __name__ == "__main__":
